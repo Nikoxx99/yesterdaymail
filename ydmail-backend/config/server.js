883c31255a3992b2566709a2eedbd540161d9b9a
Nicolas <nikoxx9900@gmail.com>
2021-08-12 15:58:01 -0500

    NuxtJS app & strapi initialization

---
+++ ydmail-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '98925165486d6391190540ccda870d56'),
+    },
+  },
+});
---
+++ ydmail-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '35312981-f8d0-4181-8acc-6f770f17ea02'
+};
